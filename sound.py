fc393ff97a35931fe345d3adbf32a3c4a2794c6b
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-13 23:39:23 -0500

    [Easteregg] Added some romeo baby

--- sound.py
+++ sound.py
@@ -7,3 +7,5 @@ class Sound:
         pg.mixer.init()
         self.path = 'resources/sound/'
         self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
+        self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
+        #self.theme = pg.mixer.music.load(self.path + 'noche_de_senso.mp3')
