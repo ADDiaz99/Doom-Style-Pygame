75582ed87ba8ee2e546133eb0dff51e13d556357
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-13 23:32:59 -0500

    Added Sound files and Shotgun sound

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from object_renderer import *
 from sprite_object import *
 from object_handler import *
 from weapon import *
+from sound import *
 # Cabeza de ahuyama
 
 
@@ -27,6 +28,7 @@ class Game:
         self.raycasting = Raycasting(self)
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
+        self.sound = Sound(self)
 
     def update(self):
         self.player.update()
